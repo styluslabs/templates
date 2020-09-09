34c26106b58aa9d64c54739624d24850c5d6f74f
imswebra <eric@imswebra.com>
2020-09-08 20:42:46 -0600

    Add support for filenames with spaces

--- pdf2write.sh
+++ pdf2write.sh
@@ -2,7 +2,7 @@
 # Create a Write document from a PDF by generating page images
 
 pdf2images() {
-  (command -v pdftoppm >/dev/null 2>&1 && pdftoppm -png -r 300 $1 out) || convert -density 300 -scene 1 $1 out-%03d.png
+  (command -v pdftoppm >/dev/null 2>&1 && pdftoppm -png -r 300 "$1" out) || convert -density 300 -scene 1 "$1" out-%03d.png
   if [ ! -f "out-1.png" ] && [ ! -f "out-01.png" ] && [ ! -f "out-001.png" ]; then
     echo "No page images found: make sure pdftoppm (from poppler-utils) or imagemagick and ghostscript are installed"
     exit 1
@@ -10,8 +10,8 @@ pdf2images() {
 }
 
 images2write() {
-  printf '<svg id="write-document" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n' > $1
-  printf '<rect id="write-doc-background" width="100%%" height="100%%" fill="#808080"/>\n' >> $1
+  printf '<svg id="write-document" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n' > "$1"
+  printf '<rect id="write-doc-background" width="100%%" height="100%%" fill="#808080"/>\n' >> "$1"
 
   local pngpage
   for pngpage in out-*.png
@@ -23,20 +23,19 @@ images2write() {
     local width=$((width2/2))
     local height=$((height2/2))
 
-    printf '<svg class="write-page" color-interpolation="linearRGB" x="10" y="10" width="%dpx" height="%dpx" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n' $width $height >> $1
-    printf '  <g class="write-content write-v3" xruling="0" yruling="40" marginLeft="100" papercolor="#FFFFFF" rulecolor="#9F0000FF">\n' >> $1
-    printf '    <g class="ruleline write-no-dup" shape-rendering="crispEdges">\n' >> $1
-    printf '      <rect class="pagerect" fill="#FFFFFF" stroke="none" x="0" y="0" width="%d" height="%d" />\n' $width $height >> $1
-
-    printf '      <image x="0" y="0" width="%d" height="%d" xlink:href="data:image/png;base64,' $width $height >> $1
+    printf '<svg class="write-page" color-interpolation="linearRGB" x="10" y="10" width="%dpx" height="%dpx" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n' $width $height >> "$1"
+    printf '  <g class="write-content write-v3" xruling="0" yruling="40" marginLeft="100" papercolor="#FFFFFF" rulecolor="#9F0000FF">\n' >> "$1"
+    printf '    <g class="ruleline write-no-dup" shape-rendering="crispEdges">\n' >> "$1"
+    printf '      <rect class="pagerect" fill="#FFFFFF" stroke="none" x="0" y="0" width="%d" height="%d" />\n' $width $height >> "$1"
+    printf '      <image x="0" y="0" width="%d" height="%d" xlink:href="data:image/png;base64,' $width $height >> "$1"
     # doesn't seem to be a way to prevent base64 from appending newline
-    base64 $pngpage | tr -d '\n' >> $1
-    printf '"/>\n    </g>\n  </g>\n</svg>' >> $1
+    base64 $pngpage | tr -d '\n' >> "$1"
+    printf '"/>\n    </g>\n  </g>\n</svg>' >> "$1"
   done
 
-  printf "\n</svg>" >> $1
+  printf "\n</svg>" >> "$1"
 
-  gzip -S z $1
+  gzip -S z "$1"
   rm out-*.png
 }
 
@@ -57,10 +56,10 @@ do
   fi
 
   echo "Converting $PDF to images..."
-  pdf2images $PDF
+  pdf2images "$PDF"
 
   echo "Generating Write document..."
-  SVGOUT=$(basename $PDF pdf)svg
-  images2write $SVGOUT
+  SVGOUT=$(basename "$PDF" pdf)svg
+  images2write "$SVGOUT"
   echo "Finished creating ${SVGOUT}z"
 done
